429ffc4faf02cd310dac149f5eaba32cfe6ebc53
Reda <redanabil2199@gmail.com>
2023-01-10 16:53:19 +0100

    admin logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '**', component: WelcomeComponent },
 ];
 
--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  auth = new FirebaseTSAuth();
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    if (!this.auth.isSignedIn()) {
+      this.router.navigate(['']);
+    }
   }
-
+  onLogoutClick() {
+    this.auth.signOut();
+    if (this.auth.isSignedIn()) {
+      return;
+    } else {
+      this.router.navigate(['']);
+    }
+  }
 }
